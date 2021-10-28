866a86f4637ccb7e23ca784346c706af406b8692
Neha2486 <nehasingh.iabm@gmail.com>
2021-10-27 21:36:03 -0500

    trigger auto 1

--- src/test/java/PipelineTest/S14NavigatorDemo.java
+++ src/test/java/PipelineTest/S14NavigatorDemo.java
@@ -30,7 +30,7 @@ public class S14NavigatorDemo {
 		driver.navigate().forward();
 		System.out.println(driver.getTitle());
 		Thread.sleep(2000);
-		System.out.println("this is navigation page updated for auto trigger ");
+		System.out.println("this is navigation page updated for auto trigger 2 ");
 		
 		// again open same url
 		driver.navigate().refresh();
